d28cadddf6d8f438664ba73ce91966ec5f22e163
HoustonBoston <myhorsefly12345@gmail.com>
2025-11-17 16:47:40 -0500

    Removed self keyword on the comp4600 final stack

--- cdk/comp4600_final/comp4600_final_stack.py
+++ cdk/comp4600_final/comp4600_final_stack.py
@@ -27,7 +27,7 @@ class Comp4600FinalStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # ECR
-        self.repo = ecr.Repository(
+        repo = ecr.Repository(
             self,
             "OCI-Repository",
             repository_name= RESOURCE_PREFIX + "ecr-repository",
