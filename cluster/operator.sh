231960e4042978018b5f49a2888134d0940e6e31
Gurpranked <gurpreet.singh.rehal@outlook.com>
2025-12-02 20:08:44 -0500

    chore: Change operator install script to have an explicit wait and timeout

--- cluster/operator.sh
+++ cluster/operator.sh
@@ -3,9 +3,11 @@
 helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
 && helm repo update
 
-helm install --wait --generate-name \
--n gpu-operator --create-namespace \
-nvidia/gpu-operator \
---version=v25.10.0
+helm install gpu-operator nvidia/gpu-operator \
+--namespace gpu-operator \
+--create-namespace \
+--version=v25.10.0 \
+--wait \
+--timeout 15m
 
 
