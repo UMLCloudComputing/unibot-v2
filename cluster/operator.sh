4ed58079c20ede73edae3560f39e54754f44d0af
Gurpranked <gurpreet.singh.rehal@outlook.com>
2025-12-02 20:37:25 -0500

    chore: Update operator script to avoid reinstalling drivers

--- cluster/operator.sh
+++ cluster/operator.sh
@@ -3,11 +3,14 @@
 helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
 && helm repo update
 
-helm install gpu-operator nvidia/gpu-operator \
---namespace gpu-operator \
+helm install nvidia-gpu-operator nvidia/gpu-operator \
+--namespace gpu-operator-resources \
 --create-namespace \
 --version=v25.10.0 \
 --wait \
---timeout 15m
+--timeout 15m \
+--set driver.enabled=false \
+--set toolkit.enabled=true \
+--set devicePlugin.enabled=true 
 
 
