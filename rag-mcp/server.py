8a42dacb25eed9c997961fbfdba1eda6de391c2c
Gurpranked <gurpreet.singh.rehal@outlook.com>
2026-03-27 11:35:13 -0400

    feat: Added health endpoint and refactored lifespan object management for rag-mcp server

--- rag-mcp/server.py
+++ rag-mcp/server.py
@@ -2,19 +2,23 @@
 import os
 import time
 from typing import AsyncIterator
-from ollama import Client
+from ollama import Client as OllamaClient
 from contextlib import asynccontextmanager
 from fastmcp import FastMCP, Context
 from fastmcp.server.lifespan import lifespan
 from pymilvus import MilvusClient
-from starlette.middleware.cors import CORSMiddleware
-from starlette.middleware import Middleware
+from starlette.responses import JSONResponse
+
 # Configuration
 MILVUS_URI = os.getenv("MILVUS_URI", "http://localhost:19530")
 COLLECTION_NAME = os.getenv("COLLECTION_NAME", "docs")
 OLLAMA_HOST = os.getenv("OLLAMA_HOST", "http://192.168.0.193:11434")
 TOP_K = os.getenv("TOP_K", 5)
 ADDRESS = os.getenv("ADDRESS", "127.0.0.1")
+
+# Globals
+milvus_client = None
+ollama_client = None
 
 @asynccontextmanager
 async def timeit(label: str):
@@ -28,30 +32,29 @@ async def timeit(label: str):
 # Persistent connection lifecycle
 @lifespan
 async def app_lifespan(app: FastMCP) -> AsyncIterator[dict]:
-  # Startup the connections
-  milvus = MilvusClient(uri=MILVUS_URI)
-  milvus.load_collection(COLLECTION_NAME)
-  print(f"Connected to Milvus at {MILVUS_URI}") 
-  ollama = Client(host=OLLAMA_HOST)
-  print("Connected to Ollama embedding model")
+  global milvus_client, ollama_client
 
   try:
-    yield {"milvus": milvus, "ollama": ollama}
+    # Startup the connections
+    milvus_client = MilvusClient(uri=MILVUS_URI)
+    milvus_client.load_collection(COLLECTION_NAME)
+    print(f"Connected to Milvus at {MILVUS_URI}") 
+    ollama_client = OllamaClient(host=OLLAMA_HOST)
+    print("Connected to Ollama embedding model")
+    yield
   finally:
-    milvus.release_collection(COLLECTION_NAME)
-    milvus.close()
-    
+    if milvus_client:
+      milvus_client.release_collection(COLLECTION_NAME)
+      milvus_client.close()
 
 # MCP server setup
 mcp_server = FastMCP("RAG-agentic-service", lifespan=app_lifespan)
 
-@mcp_server.tool()
-async def search_knowledge_base(query: str, ctx: Context):
-    """Searches internal vector database using provided user query"""
-    resources = ctx.lifespan_context
-    milvus_client: MilvusClient = resources["milvus"] 
-    ollama_client: Client = resources["ollama"]
 
+@mcp_server.tool()
+async def search_knowledge_base(query: str):
+    """Searches internal vector database using provided user query"""
+    global milvus_client, ollama_client 
      
     async with timeit("Ollama Embedding"):
       embeddings = ollama_client.embed(
@@ -70,6 +73,23 @@ async def search_knowledge_base(query: str, ctx: Context):
       )
    
     return {"results": results}
+
+
+@mcp_server.custom_route("/health", methods=["GET"])
+async def health_check(request):
+  global milvus_client, ollama_client 
+  if milvus_client is None:
+    return JSONResponse({"status": "uninitialized", "message": "Milvus client not connected"}, status_code=503)
+  if ollama_client is None:
+    return JSONResponse({"status": "uninitialzed", "message": "Ollama client not connected"}, status_code=503)
+  try:
+    milvus_client.list_collections()
+    ollama_client.list()
+  except Exception as e:
+    return JSONResponse({"status": "error", "message": str(e)}, status_code=503)
+  
+  return JSONResponse({"status": "healthy"})
+
 
 if __name__ == "__main__":
     mcp_server.run(transport="streamable-http", host=ADDRESS, port=8000)
